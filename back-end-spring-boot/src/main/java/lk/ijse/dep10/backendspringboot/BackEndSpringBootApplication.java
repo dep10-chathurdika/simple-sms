629fb9742b120e50e6c194372b7921263bb23a7e
Chathurdika Dayarathna <chathurdika1dayarathna@gmail.com>
2023-06-29 19:12:54 +0530

    initial back-end-spring-boot-commit

---
+++ back-end-spring-boot/src/main/java/lk/ijse/dep10/backendspringboot/BackEndSpringBootApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep10.backendspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndSpringBootApplication.class, args);
+	}
+
+}
